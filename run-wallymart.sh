af097c61968322c404a047330c851a979f43634e
Harrison Wang <harrisonized@gmail.com>
2021-08-25 15:35:37 -0700

    modify shell script to work on linux

--- run-wallymart.sh
+++ run-wallymart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e  # exit on error
 
-export CONDA=/opt/anaconda3/etc/profile.d/conda.sh
+export CONDA=~/anaconda3/etc/profile.d/conda.sh
 export CONDA_ENV=wallymart
 source $CONDA
 conda activate $CONDA_ENV
